c5be21742f73a5ac26482365c523c86d7c6a4105
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-06-06 14:46:45 +0530

    update

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<AuthProvider><App /></AuthProvider>
+    <BrowserRouter>
+    <AuthProvider>
+    <CartProvider>
+    <App />
+    </CartProvider>
+    </AuthProvider>
+    </BrowserRouter>
 
 );
 
