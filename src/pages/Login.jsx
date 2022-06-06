4d23d5561444f1677d1873814ea6ced798d90f0c
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-06-06 15:37:57 +0530

    update Authcontext done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,42 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const Login = () => {
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  
+  const {toggle}=useContext(AuthContext)
+  const navigate = useNavigate()  
+ 
+    const handleSubmit=(e)=>{
+      e.preventDefault();
+      axios.post("https://reqres.in/api/login",
+    {
+      "email":email,
+      "password": password
+    })
+    .then(function (response) {
+      console.log(response.data);
+      if(response.data){
+        toggle(true);
+        navigate("/")
+      }
+      
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    }
+  
+    
+ 
+  
   return (
     <div>
-      <input data-cy="login-email" />
-      <input data-cy="login-password" />
-      <button data-cy="login-submit"></button>
+      <input data-cy="login-email"  value={email} name="email" onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email..."/>
+      <input data-cy="login-password" value={password} name="password" onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Password..." />
+      <button data-cy="login-submit" onClick={handleSubmit}>Login</button>
     </div>
   );
 };
